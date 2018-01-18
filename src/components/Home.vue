9406f041c0a0dbfdd105f168e8d426bcebaee942
guohaijia <928331465@qq.com>
2018-01-18 20:10:22 +0800

    项目初始化和搭建简单的登录页面

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+   <h1> 登录 </h1>
+   <span>用户名</span>
+   <input type="text" v-model='username'>
+   <br>
+   <span>密码</span>
+   <input type="password" v-model='password'>
+   <br>
+   <input type="button" value="点击" @click='login'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () { }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
