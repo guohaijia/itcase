782fbab1ae542c70f70b0bf359db6891edaa4900
guohaijia <928331465@qq.com>
2018-01-21 17:10:50 +0800

    登录页完成

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,40 @@ export default {
 </script>
 
 <style>
+/*这里面的是全部都可以访问的到的*/
+@font-face {
+  font-family: 'icomoon';
+  src:  url('assets/fonts/icomoon.eot?9ugpuq');
+  src:  url('assets/fonts/icomoon.eot?9ugpuq#iefix') format('embedded-opentype'),
+    url('assets/fonts/icomoon.ttf?9ugpuq') format('truetype'),
+    url('assets/fonts/icomoon.woff?9ugpuq') format('woff'),
+    url('assets/fonts/icomoon.svg?9ugpuq#icomoon') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
 
+[class^="icon-"], [class*=" icon-"] {
+  /* use !important to prevent issues with browser extensions that change fonts */
+  font-family: 'icomoon' !important;
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+
+  /* Better Font Rendering =========== */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.icon-user-check:before {
+  content: "\e975";
+  font-size:20px;
+  line-height:40px;
+}
+.icon-key:before {
+  content: "\e98d";
+  font-size:20px;
+  line-height:40px;
+}
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,18 +2,20 @@
   <!-- 图片 -->
   <div class="info">
   <div class="avatar">
-    <img src="../assets/logo.png">
   </div>
-  <el-form class="container">
+  <!-- ref 就是类似于id 是唯一的标识 ref表示组件的名称，用于直接的操作dom -->
+  <el-form  ref="loginForm" class="container" :rules="rules" :model="form">
+  <div class="userInfo">
   <el-form-item>
-    <el-input type="text" v-model="username"></el-input>
+    <el-input prefix-icon="icon-user-check" type="text" v-model="form.username" placeholder="用户名" prop="username"></el-input>
   </el-form-item>
   <el-form-item>
-    <el-input  type="password" v-model="password"></el-input>
+    <el-input prefix-icon="icon-key" type="password" v-model="form.password" placeholder="密码" prop="password"></el-input>
   </el-form-item>
   <el-form-item>
     <el-button type="primary" @click="login">登录</el-button>
   </el-form-item>
+  </div>
 </el-form>
 </div>
 </template>
@@ -24,31 +26,49 @@ export default {
   name: '',
   data () {
     return {
-      username: '',
-      password: ''
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入完整的用户信息', trigger: 'blur' }
+        ],
+        password: [
+          {
+            required: true, message: '请输入正确的密码', trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   methods: {
     login: function () {
-      let params = {
-        username: this.username,
-        password: this.password
-      }
-      // 调用接口的函数
-      login(params).then(res => {
-        console.log(res)
-        if (res.meta.status === 200) {
-          // 请求成功的时候，服务器会给给我们一个token我们需要把token放在本地存储中
-          // 存储token
-          // 利用本地存储 setItem(key,value) 它是用追加的方式
-          localStorage.setItem('mytoken', res.data.token)
-
-          this.$router.push({
-            name: 'home'
+      // 在调用接口之前进行表单的验证操作
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          // 通过表单的验证
+          // 调用接口的函数
+          let params = {
+            username: this.form.username,
+            password: this.form.password
+          }
+          login(params).then(res => {
+            console.log(res)
+            if (res.meta.status === 200) {
+              // 请求成功的时候，服务器会给给我们一个token我们需要把token放在本地存储中
+              // 存储token
+              // 利用本地存储setItem(key,value) 它是用追加的方式
+              localStorage.setItem('mytoken', res.data.token)
+              this.$router.push({name: 'home'})
+            } else {
+              // 登陆失败 ，提示信息
+              this.$message({
+                message: res.meta.msg,
+                type: 'error'
+              })
+            }
           })
-        } else {
-          // 登陆失败 ，提示信息
-          alert('用户或者密码有误')
         }
       })
     }
@@ -60,6 +80,7 @@ export default {
 <style scoped>
 .info {
   position:absolute;
+  /*top:0; left:0; right:0;bottom:0;是一种布局的方式 ，为了实现全屏*/
   top:0;
   left:0;
   right:0;
@@ -79,9 +100,24 @@ export default {
   position:absolute;
   left:50%;
   top:110px;
+  margin-left:-70px;
   width:120px;
   height:120px;
   border:10px solid #fff;
+  border-radius:50%;
+  box-shadow:0 1px 5px #ccc;
+  overflow:hidden;
+  background-color:pink;
+  background-size:cover;
+ background-image: url('../assets/logo.png');
+  z-index:9999;
+}
+.userInfo {
+  /*固定定位是基于父盒子的定位*/
+  position:absolute;
+  width:400px;
+  margin:100px 50px;
+  text-align:center;
 }
 
 </style>
